00acc923bd90775c8e68567bf87d9285e017434c
LEEHEEWON123 <heh139811@dobedub.com>
2026-06-08 11:26:09 +0900

    feat: add Firebase Core and initialize with flutterfire options

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,12 +1,18 @@
 // flutter/lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'firebase_options.dart';
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
